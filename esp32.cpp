278526f8c39af28591bd20c56323a2e47f3ad8b5
ThiagoMenezesFieb <thiago.menezes@fbter.org.br>
2025-09-20 22:02:54 -0300

    Create esp32.cpp

---
+++ esp32.cpp
@@ -0,0 +1,30 @@
+#define bomba 13
+#define sensor_umidade 32
+const int tempo_atualizar = 100; // temporizador para aguardar mensagens do server
+volatile bool irrigar = false; // verifica se deve irrigar ou não via HTTP //implementar interrupção
+bool irrigou = false;  // flag para ver se deve irrigar
+
+void setup() {
+ Serial.begin(115200);
+ pinMode (bomba, OUTPUT);
+ digitalWrite(BOMBA, LOW);
+}
+
+int ler_umidade() {
+
+}
+
+bool irrigacao() {
+  if (irrigar) {
+    digitalWrite(BOMBA, HIGH);
+    irrigou = true;
+  }
+  return irrigar;  // retorna true se irrigou, false se não
+}
+
+
+void loop() {
+  irrigacao();
+  ler_umidade();
+}
+
