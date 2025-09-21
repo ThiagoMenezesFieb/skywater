b8acc2172055c6b1f6311ed4cb0c2566b05f2b2b
ThiagoMenezesFieb <thiago.menezes@fbter.org.br>
2025-09-20 22:23:13 -0300

    Update esp32.cpp

--- esp32.cpp
+++ esp32.cpp
@@ -6,25 +6,32 @@ bool irrigou = false;  // flag para ver se deve irrigar
 
 const char* ssid1 =""; //por nome da rede
 const char* senha1 = ""; //por senha da rede
-char* ssid2 =""; //por lógica para alteração da rede
-char* senha2 = "";
+char ssid2[32]; //por lógica para alteração da rede
+char senha2[64];
 
 void setup() {
  Serial.begin(115200);
  pinMode (bomba, OUTPUT);
- digitalWrite(BOMBA, LOW);
+ digitalWrite(bomba, LOW);
+ Serial.println("PROGRAMA INICIADO");
 }
 
 int ler_umidade() {
-
+ int leitura_sensor= analogRead(sensor_umidade); // valor bruto
+ int umidade = map(leitura_sensor, 4095, 0, 0, 100); //valor tratado de 0 a 100%
+ Serial.println(umidade);
+ return umidade;
 }
 
 bool irrigacao() {
   if (irrigar) {
-    digitalWrite(BOMBA, HIGH);
+    digitalWrite(bomba, HIGH);
     irrigou = true;
+  } else {
+    digitalWrite(bomba, LOW);   // desliga bomba
+    irrigou = false;
   }
-  return irrigar;  // retorna true se irrigou, false se não
+   return irrigou;  // retorna true se irrigou, false se não
 }
 
 
