7737c91e78493daa3da014964dfeb5717155abc3
ThiagoMenezesFieb <thiago.menezes@fbter.org.br>
2025-09-20 22:36:18 -0300

    Update esp32.cpp

--- esp32.cpp
+++ esp32.cpp
@@ -1,6 +1,9 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
 #define bomba 13
 #define sensor_umidade 32
-const int tempo_atualizar = 100; // temporizador para aguardar mensagens do server
+const int tempo_atualizar = 2000; // temporizador para aguardar mensagens do server
 volatile bool irrigar = false; // verifica se deve irrigar ou não via HTTP //implementar interrupção
 bool irrigou = false;  // flag para ver se deve irrigar
 
@@ -9,10 +12,50 @@ const char* senha1 = ""; //por senha da rede
 char ssid2[32]; //por lógica para alteração da rede
 char senha2[64];
 
+const char* mqtt_server = "test.mosquitto.org"; // broker
+const int mqtt_port = 1883;
+const char* mqtt_topic = "esp32/umidade";       // tópico para enviar dados
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("onectando ao WiFi.");
+  WiFi.begin(ssid1, senha1);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi conectado, IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop até reconectar
+  while (!client.connected()) {
+    Serial.print("Tentando conectar ao MQTT.");
+    if (client.connect("ESP32Client")) { 
+      Serial.println("conectado!");
+    } else {
+      Serial.print("falhou, rc=");
+      Serial.print(client.state());
+      Serial.println(" tentando novamente em 3s");
+      delay(3000);
+    }
+  }
+}
+
 void setup() {
  Serial.begin(115200);
  pinMode (bomba, OUTPUT);
  digitalWrite(bomba, LOW);
+
+ setup_wifi();
+ client.setServer(mqtt_server, mqtt_port);
+ 
  Serial.println("PROGRAMA INICIADO");
 }
 
@@ -36,7 +79,19 @@ bool irrigacao() {
 
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+ 
+ int umidade = ler_umidade();
   irrigacao();
-  ler_umidade();
+ 
+// Converter para string e enviar via MQTT
+  char mensagem[10];
+  snprintf(mensagem, 10, "%d", umidade);
+  client.publish(mqtt_topic, mensagem);
+
+  delay(tempo_atualizar);
 }
 
